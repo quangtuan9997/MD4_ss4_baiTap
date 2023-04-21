385b8f1fb0d4cbcbc4034fc1e989209f3e59dda0
quangtuan9997 <109707621+quangtuan9997@users.noreply.github.com>
2023-04-21 08:01:48 +0700

    Add files via upload

---
+++ ss4_baitap/ss4_baiTap.sql
@@ -0,0 +1,10 @@
+use QuanLySinhVien;
+select * from `subject` where (credit=(select max(Credit) from `subject`));
+
+select * from subject sb
+join `mark` m on sb.subId = m.subid
+ where m.mark = (select max(mark) from `mark`);
+ 
+ select s.*, avg(m.mark) from Student s
+ join mark m on s.studentId=m.studentId  
+ group by s.studentId, s.studentName;
